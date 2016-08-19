8fb43d8633b7a7f0b55e09069d3e90cabef53477
Joshua Efe <lighthouse7.0@icloud.com>
2016-08-19 17:03:09 -0400

    update

--- iGEMSCHM/genomedb/schema/admin.py
+++ iGEMSCHM/genomedb/schema/admin.py
@@ -2,15 +2,46 @@ from django.contrib import admin
 
 
 
+# Register your models here.from django.contrib import admin
+
+
+
 # Register your models here.
 
-from .models import Species
+
+from .models import HMM_profile
+
+from .models import HMM_Output
+
+from .models import Operon_Database
+
+from .models import Gene
 
 from .models import Accession
 
-admin.site.register(Species)
+from .models import Species
+
+
+admin.site.register(HMM_profile)
+
+admin.site.register(HMM_Output)
+
+admin.site.register(Operon_Database)
+
+admin.site.register(Gene)
 
 admin.site.register(Accession)
+
+admin.site.register(Species)
+
+
+
+
+
+
+
+
+
 
 
 
