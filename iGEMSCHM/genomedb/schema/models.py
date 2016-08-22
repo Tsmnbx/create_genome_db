93286a4baa99effe161d8765b67083afd13718ca
Joshua Efe <lighthouse7.0@icloud.com>
2016-08-22 14:02:25 -0400

    update with uniqness

--- iGEMSCHM/genomedb/schema/models.py
+++ iGEMSCHM/genomedb/schema/models.py
@@ -4,44 +4,44 @@ from django.db import models
 
 # Create your models here.
 class Species(models.Model):
-    key = models.CharField(max_length=200) #SpeciesKey 1111111
+    key = models.CharField(max_length=200, unique=True) #SpeciesKey 1111111
     name = models.CharField(max_length=200)
     
 class Accession(models.Model):
-    assembly_key = models.CharField(max_length=200) #AssemblyName 222222
+    assembly_key = models.CharField(max_length=200, unique=True) #AssemblyName 222222
     name = models.CharField(max_length=200)
-    species_key = models.CharField(max_length=200) #SpeciesKey 1111111
+    species_key = models.CharField(max_length=200, unique=True) #SpeciesKey 1111111
  
 class Gene(models.Model):
-    key = models.CharField(max_length=200)
+    key = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=200)
-    species_key = models.CharField(max_length=200) #SpeciesKey 1111111
-    accession_key = models.CharField(max_length=200) #AssemblyName 22222
+    species_key = models.CharField(max_length=200, unique=True) #SpeciesKey 1111111
+    accession_key = models.CharField(max_length=200, unique=True) #AssemblyName 22222
     strand = models.IntegerField()
     start = models.IntegerField()
     stop = models.IntegerField()
     genebank_annotation = models.CharField(max_length=200)
     cluster_information = models.CharField(max_length=200)
-    operon_key = models.CharField(max_length=200) # Operon key 333333333
+    operon_key = models.CharField(max_length=200, unique=True) # Operon key 333333333
     gene_order = models.IntegerField()
     
 class Operon_Database(models.Model):
-    key = models.CharField(max_length=200)  # Operon key 3333333333
+    key = models.CharField(max_length=200, unique=True, unique=True)  # Operon key 3333333333
     
-    kegg_orthology_ID = models.CharField(max_length=200)
+    kegg_orthology_ID = models.CharField(max_length=200, unique=True)
     
-    species_key = models.CharField(max_length=200) #SpeciesKey 1111111
+    species_key = models.CharField(max_length=200, unique=True) #SpeciesKey 1111111
     
     operon_name = models.CharField(max_length=200)
     
     annotation = models.CharField(max_length=200)
 
 class HMM_Output(models.Model):
-    key = models.CharField(max_length=200)
+    key = models.CharField(max_length=200, unique=True)
     
-    accession_key = models.CharField(max_length=200) #AssemblyName 222222
+    accession_key = models.CharField(max_length=200, unique=True) #AssemblyName 222222
     
-    species_key = models.CharField(max_length=200) #SpeciesKey 1111111
+    species_key = models.CharField(max_length=200, unique=True) #SpeciesKey 1111111
     
     start = models.IntegerField()
     
@@ -50,7 +50,7 @@ class HMM_Output(models.Model):
     hmm_profile_key = models.CharField(max_length=200) #HMM PROFILE KEY 4444444
 
 class HMM_Profile(models.Model):
-    key = models.CharField(max_length=200)
+    key = models.CharField(max_length=200, unique=True)
     
     name = models.CharField(max_length=200)
     
